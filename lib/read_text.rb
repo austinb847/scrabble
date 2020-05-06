a1bafda684d848a337c756bcfd83e8b2e657a58c
austinb847 <austinb847@icloud.com>
2020-05-05 17:29:35 -0700

    fix read text file method

--- lib/read_text.rb
+++ lib/read_text.rb
@@ -4,5 +4,15 @@ class TextFile
   end
   
   def read_text_file
+    @word_list = []
+    File.open(@path) do |f|
+      f.each_line do |line|
+        @word_list.push(line)
+      end
+    end
+  end
+
+  def word_list
+    @word_list
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('scrabble')
+require('read_text')
 
 describe('Word#scrabble') do
 
@@ -11,5 +12,15 @@ describe('Word#scrabble') do
   it("returns a scrabble score for a word") do
     letter = Word.new("chutzpah")
     expect(letter.scrabble()).to(eq(27))
+  end
+
+end
+
+describe('TextFile#read_text_file') do
+  it("gets a list of all words from file") do
+    file  = TextFile.new("word_list.txt")
+    file.read_text_file()
+    word_list = file.word_list
+    expect(word_list).to(eq(["Bob", "Quickly", "Ruby", "Expert", "YouTube"]))
   end
 end
